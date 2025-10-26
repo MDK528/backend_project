97222871804f7509f0a669cebddee4033163506c
Md Khalid Hossain <mdkhalidhossain528@gmail.com>
2025-10-26 14:31:47 +0530

    refactor user registration to handle cover image upload and ensure local file deletion after upload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,12 +5,10 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler (async (req, res) => {
-    // res.status(200).json({
-    //     message: "ok"
-    // })
 
     const {username, fullName, email, password} = req.body
-    console.log(`Username : ${username}`)
+    // console.log(req.body)
+    // console.log(`Username : ${username}`)
 
     if(
         [username, fullName, email, password].some((field)=>(field?.trim() === ""))
@@ -28,8 +26,14 @@ const registerUser = asyncHandler (async (req, res) => {
 
     // console.log(req.files);
 
-    const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar is required")
